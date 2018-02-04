222fb45c8d8d4a34fc208e3a73fb7fda18bf780b
Artem Skripka <therewasadrizzle@gmail.com>
2018-02-04 10:24:29 +0400

    refacor moderation/articles_controller

--- app/controllers/web/moderation/articles_controller.rb
+++ app/controllers/web/moderation/articles_controller.rb
@@ -6,7 +6,7 @@ class Web::Moderation::ArticlesController < Web::Moderation::ApplicationControll
   end
 
   def show
-    @article = find_article
+    @article = Article.find(params[:id])
   end
 
   def edit
@@ -21,15 +21,5 @@ class Web::Moderation::ArticlesController < Web::Moderation::ApplicationControll
     else
       render 'edit'
     end
-  end
-
-  private
-
-  def articles
-    Article.with_state(:on_moderation)
-  end
-
-  def find_article
-    articles.find(params[:id])
   end
 end
